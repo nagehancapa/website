31c5fec509e21cd649966d95b3ccb081c2250cd4
nagehancapa <capanagehan@gmail.com>
2021-04-23 16:26:34 +0200

    new website folder

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const listItems = document.querySelectorAll("li");
+
+console.log(`We have ${listItems.length} list items on this page`);
+
+const listItemsArray = Array.from(listItems)
+
+console.log("Hello")
+
+/* const linkElement = document.querySelector("a")
+console.log(linkElement)
+linkElement.innerText = "I've changed the innerText!"
+// linkElement.href  
+linkElement.style.backgroundColor = "blue"
+linkElement.innerHTML = "<h3>LinkdgfhdfhhdedIn</h3>"
+linkElement.innerHTML.fontsize("50px")
+console.log(linkElement)
+
+const bodyElement = document.querySelector("body")
+bodyElement.style.backgroundColor = "purple" */
